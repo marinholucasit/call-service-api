8c85b6c94a023a12f8f89f25ad31b7ff839798b7
Lucas Marinho <marinholucasit@gmail.com>
2020-09-15 09:08:59 -0300

    add method to list providers users

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import authMidd from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,8 @@ routes.post('/session', SessionController.store);
 routes.use(authMidd);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/file', upload.single('file'), FileController.store);
 
